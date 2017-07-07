1456ee8f7ed2226d065b21c7069db510e8613209
Neil Kidd <NKidd@UK.int.wan>
2017-07-07 14:05:33 +0100

    Initial Commit

---
+++ DesignPatterns/Strategy.cs
@@ -0,0 +1,53 @@
+using System;
+using Xunit;
+
+namespace DesignPatterns
+{
+    public class Strategy
+    {
+        [Fact]
+        public void Mallard_Duck_Test()
+        {
+            MallardDuck mallard = new MallardDuck();
+            Assert.Equal("looks like a Mallard", mallard.Display());
+        }
+
+        [Fact]
+        public void Redhead_Duck_Test()
+        {
+            RedheadDuck redhead = new RedheadDuck();
+            Assert.Equal("looks like a Redhead", redhead.Display());
+        }
+    }
+
+    public abstract class Duck
+    {
+        public string Quack()
+        {
+            return "Quack";
+        }
+
+        public string Swim()
+        {
+            return "Swim";
+        }
+
+        public abstract string Display();
+    }
+
+    public class MallardDuck : Duck
+    {
+        public override string Display()
+        {
+            return "looks like a Mallard";
+        }
+    }
+
+    public class RedheadDuck : Duck
+    {
+        public override string Display()
+        {
+            return "looks like a Redhead";
+        }
+    }
+}
